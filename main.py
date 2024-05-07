9e716e82d5363fc938106a920b51c106317ae395
Jacopo Ceccuti <jacopo.ceccuti@gmail.com>
2024-05-07 19:14:44 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import time
 import ntptime # type: ignore
 import socket
+from uhashlib import sha256 # type: ignore
+import ubinascii # type: ignore
 
 UTC_OFFSET = 2*60*60
 
@@ -9,14 +11,12 @@ def set_time():
         ntptime.settime()
     except:
         print("Error while setting time")
-        pass
 
 def query_time():
     try:
         return time.localtime(ntptime.time()+UTC_OFFSET)
     except:
         print("Error while querying time")
-        pass
 
 
 class FTPClient:
@@ -25,6 +25,20 @@ class FTPClient:
         self.port = port
         self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
+    def clean_hash_string(self, input_bytes):
+        input_string = input_bytes.decode('utf-8')
+        # Find the index of the first 'b'
+        
+        # Remove all occurrences of '
+        input_string = input_string.replace("'", "")
+        return input_string
+
+    def md5(self, data):
+        # Simple MD5 implementation for MicroPython
+        md5_hash = sha256()
+        md5_hash.update(data)
+        return self.clean_hash_string(ubinascii.hexlify(md5_hash.digest()))
+    
     def connect(self):
         try:
             sock_addr = socket.getaddrinfo(self.server, self.port)[0][-1]
@@ -34,7 +48,6 @@ class FTPClient:
             print(f"The server response is: {self._receive_response()}") # Visual check for connection
         except Exception as e:
             print(f"Error while connecting to server: {e}")
-            pass
 
     def send_command(self, command):
         self.sock.sendall(command.encode() + b'\r\n')
@@ -63,7 +76,6 @@ class FTPClient:
         try:
             if self.sock:
                 response = self.send_command('MKD {}'.format(directory))
-                # print(response)
                 if response.startswith('257'):
                     print(f"Directory {directory} created successfully")
                 else:
@@ -74,44 +86,67 @@ class FTPClient:
     def ftp_upload_file(self, local_file_path, remote_file_path):
         try:
             if self.sock:
+                # Read file data
+                with open(local_file_path, 'rb') as f:
+                    data = f.read()
+
                 response = self.send_command('PASV')  # Request passive mode
-                # print(response)
                 if response.startswith('227'):
                     # Split the response to get the data port
                     port_parts = response.split('(')[-1].split(')')[0].split(',')
                     # Calculate the port number using the formula (p1 * 256) + p2.
                     data_port = int(port_parts[-2]) * 256 + int(port_parts[-1]) 
-                    # print(f"Data port: {data_port}")
-                    # print(f"Port parts: {port_parts}")
                     
                     # Connect to the data port
                     data_sock = socket.socket()
                     data_sock.connect((self.server, data_port))
                     
-                    # Open the local file for reading
-                    with open(local_file_path, 'rb') as f:
-                        data = f.read()
-                    
                     print(f"Uploading local file {local_file_path} to the server...")
                     # Send the STOR command to store the file
                     self.send_command('STOR {}'.format(remote_file_path))
-                    
+
                     # Send the file data
                     data_sock.sendall(data)
+                    print("File data sent to server")
                     data_sock.close()
-                    print("File uploaded successfully to FTP server")
                 else:
                     print("Error entering passive mode")
         except Exception as e:
             print("Error uploading file to FTP server:", e)
 
+    def check_md5(self, local_file_path, remote_file_path):
+        try:
+            # Calculate MD5 checksum of local file
+            with open(local_file_path, 'rb') as f:
+                data = f.read()
+            md5_local = self.md5(data)
+
+            # Send MD5 command over the new control connection
+            md5_command = f'MD5 {md5_local} {remote_file_path}'
+            md5_response = self.send_command(md5_command)
+            
+            # Process server response accordingly
+            if md5_response.startswith('250 MD5'):
+                print("File uploaded successfully to FTP server")
+            elif md5_response.startswith('550 MD5'):
+                print("Server requested retransmission. Resending the file...")
+                self.ftp_upload_file(local_file_path, remote_file_path)
+            else:
+                print("Error uploading file to FTP server")
+            
+        except Exception as e:
+            print("Error checking MD5 checksum:", e)
+
 set_time()
 actual_time = query_time()
-print(f"\nToday is {actual_time[2]}/{actual_time[1]}/{actual_time[0]} and it is {actual_time[3]}:{actual_time[4]}:{actual_time[5]}")
+if actual_time is not None:
+    print(f"\nToday is {actual_time[2]}/{actual_time[1]}/{actual_time[0]} and it is {actual_time[3]}:{actual_time[4]}:{actual_time[5]}")
 
 ftp = FTPClient('172.20.10.2', 2121)
 ftp.connect()
 ftp.login('Jacopo', 'pippetto')
 ftp.ftp_create_directory("Test")
-ftp.ftp_upload_file("Files/Test.txt", "Test\\file.txt")
+ftp.ftp_upload_file("Files/LAT CABINA_001_240501_155139_EVENT_001.txt", "Test\\LAT CABINA_001_240501_155139_EVENT_001.txt")
+ftp.ftp_upload_file("Files/LAT CABINA_001_240501_155729_EVENT_002.txt", "Test\\LAT CABINA_001_240501_155729_EVENT_002.txt")
+ftp.check_md5("Files/LAT CABINA_001_240501_155729_EVENT_002.txt", "Test\\LAT CABINA_001_240501_155729_EVENT_002.txt") 
 ftp.quit()
